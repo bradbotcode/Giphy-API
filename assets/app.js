e105d80f6032af7cc4038dc0ef880d6a625ba77f
brad ferrell <ferrellbh@gmail.com>
2018-03-09 09:22:45 -0500

    comment update

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,7 @@ function displayGifs() {
     var result = response.data;
 
     //for loop to create new div, the gif rating, & the gif url
-    //loop also creates a data attribute for both still and animated version of gif along with a data attribute set to "animate"
+    //loop also creates a data attribute for both still and animated version of gif along with a data attribute for current state
     for (i = 0; i < result.length; i++) {
       var newDiv = $(
         "<div class='col-3'><p class='hack'>Rating: " +
